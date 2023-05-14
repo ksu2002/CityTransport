f5cf06a1b73f943bd9a7dd26ca9d09077198bc73
ksu2002 <simonova278@gmail.com>
2023-05-14 22:10:14 +0500

    Добавьте файлы проекта.

---
+++ CityTransportWork/Auth.Designer.cs
@@ -0,0 +1,121 @@
+﻿
+namespace CityTransportWork
+{
+    public partial class Auth
+    {
+        
+
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.loginBox = new System.Windows.Forms.TextBox();
+            this.passwordBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.authButton = new System.Windows.Forms.Button();
+            this.statusAuth = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // loginBox
+            // 
+            this.loginBox.Location = new System.Drawing.Point(296, 134);
+            this.loginBox.Name = "loginBox";
+            this.loginBox.Size = new System.Drawing.Size(100, 22);
+            this.loginBox.TabIndex = 0;
+            this.loginBox.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // passwordBox
+            // 
+            this.passwordBox.Location = new System.Drawing.Point(296, 202);
+            this.passwordBox.Name = "passwordBox";
+            this.passwordBox.Size = new System.Drawing.Size(100, 22);
+            this.passwordBox.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(177, 140);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Логин";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(177, 208);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Пароль";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // authButton
+            // 
+            this.authButton.Location = new System.Drawing.Point(274, 342);
+            this.authButton.Name = "authButton";
+            this.authButton.Size = new System.Drawing.Size(75, 23);
+            this.authButton.TabIndex = 4;
+            this.authButton.Text = "Войти";
+            this.authButton.UseVisualStyleBackColor = true;
+            this.authButton.Click += new System.EventHandler(this.authButton_Click);
+            // 
+            // statusAuth
+            // 
+            this.statusAuth.AutoSize = true;
+            this.statusAuth.Location = new System.Drawing.Point(296, 280);
+            this.statusAuth.Name = "statusAuth";
+            this.statusAuth.Size = new System.Drawing.Size(0, 16);
+            this.statusAuth.TabIndex = 5;
+            this.statusAuth.Click += new System.EventHandler(this.statusAuth_Click);
+            // 
+            // Auth
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.statusAuth);
+            this.Controls.Add(this.authButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.passwordBox);
+            this.Controls.Add(this.loginBox);
+            this.Name = "Auth";
+            this.Text = "Auth";
+            this.Load += new System.EventHandler(this.Auth_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox loginBox;
+        private System.Windows.Forms.TextBox passwordBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button authButton;
+        private System.Windows.Forms.Label statusAuth;
+    }
+}
---
+++ CityTransportWork/Auth.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace CityTransportWork
+{
+    public partial class Auth : Form
+    {
+        private SqlConnectionStringBuilder sqlConnectionStringBuilder = new SqlConnectionStringBuilder()
+        {
+            DataSource = "DESKTOP-H2QPSJ9\\SQLEXPRESS",
+            InitialCatalog = "CityTransport",
+            PersistSecurityInfo = false,
+            TrustServerCertificate = true,
+        };
+
+        public Auth()
+        {
+            InitializeComponent();
+        }
+
+        private void Auth_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void authButton_Click(object sender, EventArgs e)
+        {
+            sqlConnectionStringBuilder.UserID = "guest";
+            sqlConnectionStringBuilder.Password = "123";
+
+            SqlConnection sqlConnection = new SqlConnection(sqlConnectionStringBuilder.ConnectionString);
+     
+            try
+            {
+                SqlCommand sqlCommand = new SqlCommand($"dbo.[auth] {loginBox.Text},{passwordBox.Text}", sqlConnection);
+                sqlConnection.Open();
+                SqlDataReader auth = sqlCommand.ExecuteReader();
+                if (auth.Read())
+                {
+                    sqlConnectionStringBuilder.UserID = auth.GetValue(0).ToString();
+                    sqlConnectionStringBuilder.Password = auth.GetValue(1).ToString();
+                    statusAuth.Text = "Подключение успешно";
+                }
+                else
+                {
+                    statusAuth.Text = "Неверен логин или пароль";
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return;
+            }
+            finally
+            {
+                sqlConnection.Close();
+            }
+            if(sqlConnectionStringBuilder.UserID == "driver")
+            {
+                statusAuth.Text = "водитель";
+            }
+            if (sqlConnectionStringBuilder.UserID == "passenger")
+            {
+                statusAuth.Text = "passenger";
+            }
+            if (sqlConnectionStringBuilder.UserID == "scheduler")
+            {
+                statusAuth.Text = "scheduler";
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void statusAuth_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
