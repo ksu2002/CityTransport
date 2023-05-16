12a9403576106d98b8ddb9ee3193eb07445e91d1
ksu2002 <simonova278@gmail.com>
2023-05-16 19:32:04 +0500

    passenger form showroutes()

--- CityTransportWork/PassengerForm.Designer.cs
+++ CityTransportWork/PassengerForm.Designer.cs
@@ -41,7 +41,7 @@
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(0, 33);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowHeadersWidth = 35;
             this.dataGridView1.RowTemplate.Height = 24;
             this.dataGridView1.Size = new System.Drawing.Size(1172, 459);
             this.dataGridView1.TabIndex = 0;
--- CityTransportWork/PassengerForm.cs
+++ CityTransportWork/PassengerForm.cs
@@ -25,7 +25,50 @@ namespace CityTransportWork
         {
             InitializeComponent();
         }
+        private void ShowRoutes()
+        {
+            sqlConnectionStringBuilder.UserID = "passenger";
+            sqlConnectionStringBuilder.Password = "passenger";
+            try
+            {
+                SqlConnection sqlConnection = new SqlConnection(sqlConnectionStringBuilder.ConnectionString);
+                string stop1name = stop1.SelectedItem.ToString();
+                string stop2name = stop2.SelectedItem.ToString();
 
+                string query1 = ($"SELECT*FROM dbo.RoutsFromStop1ToStop2({"'" + stop1name + "'"}, {"'" + stop2name + "'"})");
+                label1.Text = stop1name;
+                label2.Text = stop2name;
+                SqlCommand command1 = new SqlCommand(query1, sqlConnection);
+                SqlDataAdapter dataAdapter1 = new SqlDataAdapter();
+                dataAdapter1.SelectCommand = command1;
+
+                DataSet dataSet = new DataSet();
+                int check = dataAdapter1.Fill(dataSet);
+                if (check != 0)
+                {
+                    dataGridView1.DataSource = dataSet.Tables[0];
+                }
+                else
+                {
+                    string query2 = ($"SELECT*FROM dbo.TransferFromStop1ToStop2({"'" + stop1name + "'"}, {"'" + stop2name + "'"})");
+                    label1.Text = stop1name;
+                    label2.Text = stop2name;
+                    SqlCommand command2 = new SqlCommand(query2, sqlConnection);
+                    SqlDataAdapter dataAdapter2 = new SqlDataAdapter();
+                    dataAdapter2.SelectCommand = command2;
+                    DataSet dataSet1 = new DataSet();
+                    dataAdapter2.Fill(dataSet1);
+                    dataGridView1.DataSource = dataSet1.Tables[0];
+                }
+                sqlConnection.Close();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return;
+            }
+        }
         private void PassengerForm_Load(object sender, EventArgs e)
         {
             sqlConnectionStringBuilder.UserID = "passenger";
@@ -38,29 +81,21 @@ namespace CityTransportWork
             {
                
                 SqlCommand sqlCommand1 = new SqlCommand($"dbo.[getStopName]", sqlConnection);
-    
                 SqlDataReader reader1 = sqlCommand1.ExecuteReader();
                 while (reader1.Read())
                 {
                     stop1.Items.Add(reader1.GetString(0));
                 }
-
-                // Закрытие SqlDataReader
                 reader1.Close();
-
                 SqlCommand sqlCommand2 = new SqlCommand($"dbo.[getStopName]", sqlConnection);
-
                 SqlDataReader reader2 = sqlCommand2.ExecuteReader();
                 while (reader2.Read())
                 {
                     stop2.Items.Add(reader2.GetString(0));
                 }
 
-                // Закрытие SqlDataReader
                 reader2.Close();
-
                 sqlConnection.Close();
-
             }
             catch (Exception ex)
             {
@@ -73,76 +108,19 @@ namespace CityTransportWork
         {
 
         }
-        private int stop1_id = 1;
-        private int stop2_id = 7;
-        private string stop1name;
-        private string stop2name;
         private void stop1_SelectedIndexChanged(object sender, EventArgs e)
         {
-           
-            ///   try
-            // {
-            //    SqlConnection sqlConnection = new SqlConnection(sqlConnectionStringBuilder.ConnectionString);
-
-            //string stop1name = stop1.SelectedItem.ToString();
-        /*       SqlCommand sqlCommand = new SqlCommand($"dbo.[getStopID] {"'" + stop1name + "'"}", sqlConnection);
-                sqlConnection.Open();
-                SqlDataReader reader = sqlCommand.ExecuteReader();
-
-                if (reader.Read())
-                {
-                    int stop1_id = Int32.Parse(reader.GetValue(0).ToString());
-
-                }
-                reader.Close();
-                sqlConnection.Close();
-            }
-
-            catch (Exception ex)
+            if  (stop1.SelectedIndex != -1 && stop2.SelectedIndex != -1)
             {
-                MessageBox.Show(ex.Message);
-                return;
-            }*/
-            
-
+                ShowRoutes();
+            }
         }
 
         private void stop2_SelectedIndexChanged(object sender, EventArgs e)
         {
-            sqlConnectionStringBuilder.UserID = "passenger";
-            sqlConnectionStringBuilder.Password = "passenger";
-            try
+            if (stop1.SelectedItem.ToString() != null && stop2.SelectedItem.ToString() != null)
             {
-                SqlConnection sqlConnection = new SqlConnection(sqlConnectionStringBuilder.ConnectionString);
-                string stop1name = stop1.SelectedItem.ToString();
-                string stop2name = stop2.SelectedItem.ToString();
-          /*      SqlCommand sqlCommand = new SqlCommand($"dbo.[getStopID] {"'" + stop2name + "'"}", sqlConnection);
-                sqlConnection.Open();
-                SqlDataReader reader = sqlCommand.ExecuteReader();
-
-                if (reader.Read())
-                {
-                    int stop2_id = Int32.Parse(reader.GetValue(0).ToString());
-                }
-                reader.Close();
-           */
-                string query = ($"SELECT*FROM dbo.RoutsFromStop1ToStop2({"'" + stop1name + "'"}, {"'" + stop2name + "'"})");
-                label1.Text = stop1name;
-                label2.Text = stop2name;
-                SqlCommand command = new SqlCommand(query, sqlConnection);
-                SqlDataAdapter dataAdapter = new SqlDataAdapter();
-                dataAdapter.SelectCommand = command;
-
-                DataSet dataSet = new DataSet();
-                dataAdapter.Fill(dataSet);
-                dataGridView1.DataSource = dataSet.Tables[0];
-                sqlConnection.Close();
-
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-                return;
+                ShowRoutes();
             }
         }
     }
